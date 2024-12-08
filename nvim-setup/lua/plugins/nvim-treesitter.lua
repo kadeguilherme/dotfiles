68b2c83939c50fa9478f4d73902ad718d72d41dd
Guilherme <kadeguilherme@gmail.com>
2024-12-08 11:34:40 -0300

    install treesitter

---
+++ nvim-setup/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,35 @@
+-------------------------------------------------
+-- name : nvim-web-devicons
+-- url  : https://github.com/nvim-tree/nvim-web-devicons
+-------------------------------------------------
+local config = function()
+	require("nvim-treesitter.configs").setup({
+		indent = {
+			enable = true,
+		},
+		autotag = {
+			enable = true,
+		},
+		ensure_installed = {
+			"markdown",
+			"json",
+			"yaml",
+			"html",
+			"lua",
+			"dockerfile",
+			"gitignore",
+			"python",
+		},
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = true,
+		},
+	})
+end
+
+return {
+	"nvim-treesitter/nvim-treesitter",
+  	lazy = false,
+  	config = config
+}
