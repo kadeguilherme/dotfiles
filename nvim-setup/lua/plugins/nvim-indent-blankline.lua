7dae25042883fca28b868354a0832a1c8a4e499b
Guilherme <kadeguilherme@gmail.com>
2024-12-08 12:58:09 -0300

    install indent-blankline

---
+++ nvim-setup/lua/plugins/nvim-indent-blankline.lua
@@ -0,0 +1,13 @@
+-------------------------------------------------
+-- name : indent-blankline.nvim
+-- url  : https://github.com/lukas-reineke/indent-blankline.nvim
+-------------------------------------------------
+return {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        lazy = false,
+
+        ---@module "ibl"
+        ---@type ibl.config
+        opts = {},
+}
--- nvim-setup/lua/util/init.lua
+++ nvim-setup/lua/util/init.lua
@@ -4,5 +4,7 @@ require("plugins.nvim-tree")
 require("plugins.nvim-icon")
 require('plugins.nvim-telescope')
 require('plugins.nvim-lualine')
+require('plugins.nvim-indent-blankline')
+
 --- Config
 require("config")
