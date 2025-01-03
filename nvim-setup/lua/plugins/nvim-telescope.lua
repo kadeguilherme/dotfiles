a31e93d6c17027466be474627ea89430792dda51
kadeguilherme <kadeguilherme@gmail.com>
2025-01-03 16:30:21 -0300

    Configuracao do telescope

--- nvim-setup/lua/plugins/nvim-telescope.lua
+++ nvim-setup/lua/plugins/nvim-telescope.lua
@@ -1,51 +1,50 @@
 -------------------------------------------------
--- name : telescope
--- url  : https://github.com/nvim-telescope/telescope.nvim
--------------------------------------------------
-
-local mapkey = require("util.keymapper").mapkey
-
-local config = function()
-	local telescope = require("telescope")
-	telescope.setup({
-		defaults = {
-			mappings = {
-				i = {
-					["<C-j>"] = "move_selection_next",
-					["<C-k>"] = "move_selection_previous",
-				},
-			},
-		},
-		pickers = {
-			find_files = {
-				theme = "dropdown",
-				previewer = false,
-				hidden = true,
-			},
-			live_grep = {
-				theme = "dropdown",
-				previewer = false,
-			},
-			buffers = {
-				theme = "dropdown",
-				previewer = false,
-			},
-		},
-	})
-end
-
+-- name : vim-tmux-navigator
+-- url  : https://github.com/christoomey/vim-tmux-navigator
+-----------------------------------------------
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = config,
-	keys = {
-		mapkey("<leader>fk", "Telescope keymaps", "n"),
-		mapkey("<leader>fh", "Telescope help_tags", "n"),
-		mapkey("<leader>ff", "Telescope find_files", "n"), --'Telescope find files
-		mapkey("<leader>fg", "Telescope live_grep", "n"), --Telescope live grep
-		mapkey("<leader>fb", "Telescope buffers", "n"),
-    mapkey("<leader>fr", "Telescope git_branches", "n"),--Telescope find branch
-	},
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+
+      telescope.setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({})
+          },
+        },
+        pickers = {
+			    find_files = {
+				    theme = "dropdown",
+				    previewer = false,
+				    hidden = true,
+			    },
+			    live_grep = {
+				    --theme = "dropdown",
+				    --previewer = true,
+            --layout_strategy = "horizontal",
+            preview_width = 0.6,
+			    },
+			    buffers = {
+				    theme = "dropdown",
+				    previewer = false,
+			    },
+		    },
+      })
+      vim.keymap.set('n', '<leader>ff', builtin.find_files,{ desc = 'Telescope find files' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set('n', '<leaer>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fr', builtin.git_branches, { desc = 'Telescope git branches' })
+
+      telescope.load_extension("ui-select")
+    end,
+  },
+  {
+    'nvim-telescope/telescope-ui-select.nvim',
+  },
 }
