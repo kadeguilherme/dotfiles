76cdce777c7dfd7fa7122918f14c8b1263c814e8
shikamaru <kadeguilherme@gmail.com>
2026-08-02 12:16:26 -0300

    feat(plugins): cord.nvim e tmux-navigator

---
+++ nvim/lua/plugins/cord.lua
@@ -0,0 +1,40 @@
+-- cord.nvim
+-- https://github.com/vyfor/cord.nvim
+-- Discord Rich Presence integration for Neovim.
+return {
+  "vyfor/cord.nvim",
+  event = "VeryLazy",
+  opts = {
+    editor = {
+      client = "neovim",
+      tooltip = "Building things with Neovim",
+    },
+
+    display = {
+      swap_fields = true,
+    },
+
+    timestamp = {
+      enabled = true,
+      reset_on_change = false,
+    },
+
+    text = {
+      viewing = function()
+        return "Coding in Neovim"
+      end,
+
+      workspace = function()
+        return "Neovim"
+      end,
+    },
+
+    assets = {
+      lazy = {
+        icon = "https://raw.githubusercontent.com/folke/lazy.nvim/main/.github/logo.svg",
+        name = "lazy.nvim",
+        tooltip = "The lazy plugin manager",
+      },
+    },
+  },
+}
