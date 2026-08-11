dd6213a67509e978e5cc3c19db34e4eebda007f8
shikamaru <kadeguilherme@gmail.com>
2026-08-11 13:10:49 -0300

    adicionando checkmate.lua

---
+++ nvim/lua/plugins/checkmate.lua
@@ -0,0 +1,9 @@
+-- checkmate.nvim
+-- https://github.com/bngarren/checkmate.nvim
+-- Manage markdown checklists: toggle tasks and track progress.
+return {
+  "bngarren/checkmate.nvim",
+  ft = "markdown",
+
+  opts = {},
+}
