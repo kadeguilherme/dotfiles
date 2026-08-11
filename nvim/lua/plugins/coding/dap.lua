609018d8b801bceffe61eeee7985cbbb9ddf0d77
shikamaru <kadeguilherme@gmail.com>
2026-08-11 13:59:17 -0300

    adicioando o plugin dap e lual.lua

---
+++ nvim/lua/plugins/coding/dap.lua
@@ -0,0 +1,134 @@
+-- nvim-dap
+-- https://github.com/mfussenegger/nvim-dap
+-- Debug Adapter Protocol client: breakpoints, stepping and REPL.
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    { "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
+    "theHamsta/nvim-dap-virtual-text",
+    { "leoluz/nvim-dap-go", ft = "go" },
+  },
+  -- Grupo <leader>d = Debug
+  keys = {
+    {
+      "<leader>db",
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      desc = "Debug: breakpoint",
+    },
+    {
+      "<leader>dB",
+      function()
+        require("dap").set_breakpoint(vim.fn.input("Condição do breakpoint: "))
+      end,
+      desc = "Debug: breakpoint condicional",
+    },
+    {
+      "<leader>dc",
+      function()
+        require("dap").continue()
+      end,
+      desc = "Debug: continuar/iniciar",
+    },
+    {
+      "<leader>di",
+      function()
+        require("dap").step_into()
+      end,
+      desc = "Debug: step into",
+    },
+    {
+      "<leader>do",
+      function()
+        require("dap").step_over()
+      end,
+      desc = "Debug: step over",
+    },
+    {
+      "<leader>dO",
+      function()
+        require("dap").step_out()
+      end,
+      desc = "Debug: step out",
+    },
+    {
+      "<leader>dr",
+      function()
+        require("dap").repl.toggle()
+      end,
+      desc = "Debug: REPL",
+    },
+    {
+      "<leader>dl",
+      function()
+        require("dap").run_last()
+      end,
+      desc = "Debug: rodar último",
+    },
+    {
+      "<leader>dt",
+      function()
+        require("dap").terminate()
+      end,
+      desc = "Debug: terminar",
+    },
+    {
+      "<leader>du",
+      function()
+        require("dapui").toggle()
+      end,
+      desc = "Debug: alternar UI",
+    },
+    {
+      "<leader>de",
+      function()
+        require("dapui").eval()
+      end,
+      mode = { "n", "v" },
+      desc = "Debug: avaliar expressão",
+    },
+    -- Go (delve)
+    {
+      "<leader>dgt",
+      function()
+        require("dap-go").debug_test()
+      end,
+      desc = "Debug: teste Go sob o cursor",
+    },
+    {
+      "<leader>dgl",
+      function()
+        require("dap-go").debug_last_test()
+      end,
+      desc = "Debug: último teste Go",
+    },
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    dapui.setup()
+    require("nvim-dap-virtual-text").setup()
+    require("dap-go").setup() -- configura o adapter do delve (dlv) automaticamente
+
+    -- Abre/fecha a UI junto com a sessão de debug
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    -- Sinais na gutter
+    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticError", numhl = "" })
+    vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "DiagnosticWarn", numhl = "" })
+    vim.fn.sign_define("DapStopped", { text = "", texthl = "DiagnosticWarn", linehl = "Visual", numhl = "" })
+  end,
+}
